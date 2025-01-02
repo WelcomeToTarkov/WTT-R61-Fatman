fa1c91baa5b8d49af7abdcb98e73b82b173bca6d
madmanbeavisx <beavis@madmanbeavis.com>
2025-01-01 22:54:30 -0500

    Initial

---
+++ dist/src/mod.ts
@@ -0,0 +1,77 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { DependencyContainer } from "tsyringe";
+import { IPreSptLoadMod } from "@spt/models/external/IPreSptLoadMod";
+import { LogTextColor } from "@spt/models/spt/logging/LogTextColor";
+import { WTTInstanceManager } from "./WTTInstanceManager";
+import { CustomItemService } from "./CustomItemService";
+import { CustomAssortSchemeService } from "./CustomAssortSchemeService";
+
+class Mod implements IPreSptLoadMod {
+    private Instance: WTTInstanceManager = new WTTInstanceManager();
+    private version: string;
+    private modName = "WTT-R61 Fatman";
+
+    private customItemService: CustomItemService = new CustomItemService();
+    private customAssortSchemeService: CustomAssortSchemeService = new CustomAssortSchemeService();
+
+    debug = false;
+
+    // Anything that needs done on preSptLoad, place here.
+    public preSptLoad(container: DependencyContainer): void {
+        // Initialize the instance manager DO NOTHING ELSE BEFORE THIS
+        this.Instance.preSptLoad(container, this.modName);
+        this.Instance.debug = this.debug;
+        // EVERYTHING AFTER HERE MUST USE THE INSTANCE
+
+        this.displayCreditBanner();
+
+        this.customItemService.preSptLoad(this.Instance);
+        this.customAssortSchemeService.preSptLoad(this.Instance);
+
+    }
+
+    postDBLoad(container: DependencyContainer): void {
+        // Initialize the instance manager DO NOTHING ELSE BEFORE THIS
+        this.Instance.postDBLoad(container);
+        // EVERYTHING AFTER HERE MUST USE THE INSTANCE
+
+        this.customItemService.postDBLoad();
+        this.customAssortSchemeService.postDBLoad();
+
+        this.Instance.logger.log(
+            `[${this.modName}] Database: Loading complete.`,
+            LogTextColor.GREEN
+        );
+    }
+
+    public colorLog(message: string, color: string) {
+        const colorCodes = {
+            red: "\x1b[31m",
+            green: "\x1b[32m",
+            yellow: "\x1b[33m",
+            blue: "\x1b[34m",
+            magenta: "\x1b[35m",
+            cyan: "\x1b[36m",
+            white: "\x1b[37m",
+            gray: "\x1b[90m",
+            brightRed: "\x1b[91m",
+            brightGreen: "\x1b[92m",
+            brightYellow: "\x1b[93m",
+            brightBlue: "\x1b[94m",
+            brightMagenta: "\x1b[95m",
+            brightCyan: "\x1b[96m",
+            brightWhite: "\x1b[97m"
+        };
+
+        const resetCode = "\x1b[0m";
+        const colorCode = colorCodes[color as keyof typeof colorCodes] || "\x1b[37m"; // Default to white if color is invalid.
+        console.log(`${colorCode}${message}${resetCode}`); // Log the colored message here
+    }
+
+    private displayCreditBanner(): void {
+        this.colorLog(`[${this.modName}] Developers:  MadManBeavis                 Code Framework: GroovypenguinX
+                                                        Laugh and Grow Fat.`, "green");
+    }
+}
+
+export const mod = new Mod();
